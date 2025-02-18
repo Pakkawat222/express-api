f1037a25fadbafcfccc4b4d9df2d22783e566249
Pakkawat222 <pakkawat.bun@dome.tu.ac.th>
2025-02-18 12:34:36 +0700

    2

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
 import Counter from "./Counter";
 import ProductList from "./ProductList";
+import { useState, useEffect } from "react";
 function App() {
   return (
     <div>
@@ -15,6 +16,22 @@ function App() {
     <div>
       <h1>React Debugging</h1>
       <ProductList />
+    </div>
+  );
+}
+function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/products")
+      .then(res => res.json())
+      .then(data => setProducts(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Product List</h1>
+      {products.map(p => <p key={p._id}>{p.name} - ${p.price}</p>)}
     </div>
   );
 }
